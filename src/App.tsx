694d767068a96047b59ea63371324d2f40492eae
rioxraman <ramanpkgupta@gmail.com>
2023-07-11 23:42:48 +0530

    compontents

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,29 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import ListGroup from './components/ListGroup';
+import Alert from './components/Alert';
+import Button from './components/Button';
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    const [count, setCount] = useState(0);
+    const [alertVisiblity, setAlertVisiblity] = useState(false);
+    let items = ['newyork', 'california', 'florida', 'colarado', 'arizona'];
+    const handleSelectedItem = (item: string) => {
+        console.log(item);
+    };
+    return (
+        <div>
+            <ListGroup items={items} heading="cities" onSelectItem={handleSelectedItem} />
+            {alertVisiblity && <Alert onClose={() => setAlertVisiblity(false)}>Hello Owlrd</Alert>}
+            <Button
+                onClick={() => {
+                    setAlertVisiblity(true);
+                }}
+                color="secondary"
+            >
+                Click Here
+            </Button>
+        </div>
+    );
 }
 
 export default App
